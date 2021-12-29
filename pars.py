3ad49bec162c471537fb02008e9f3e1964eb06f6
Kostia2004 <k.a.kudriavsev@mail.ru>
2021-12-29 21:19:22 +0300

    SITE_URL in table

--- pars.py
+++ pars.py
@@ -2,23 +2,26 @@ import requests
 import time
 import random
 
-url= 'https://api.hh.ru/vacancies/'
-result_url = 'https://hh.ru/vacancy/'
+API_URL = 'https://api.hh.ru/vacancies/'
+SITE_URL = 'https://hh.ru/vacancy/'
+ID = 35787520
 
-for i in range(35787520, 1, -1):
+while True:
+    ID += 1
     f = open('list.csv', 'a')
-    d = requests.get(url+str(i)).json()
+    d = requests.get(API_URL+str(ID)).json()
     print()
     try:
         if (d["salary"])["from"]==None:
-            f.write(str(d["name"])+';'+str((d["salary"])["to"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+url+str(i)+'\n')
-            print(d["name"], (d["salary"])["to"], (d["salary"])["currency"], d['created_at']+' '+url+str(i))
+            f.write(str(d["name"])+';'+str((d["salary"])["to"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+SITE_URL+str(ID)+'\n')
+            print(d["name"], (d["salary"])["to"], (d["salary"])["currency"], d['created_at']+' '+SITE_URL+str(ID))
         elif (d["salary"])["to"]==None:
-            f.write(str(d["name"])+';'+str((d["salary"])["from"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+url+str(i)+'\n')
-            print(d["name"], (d["salary"])["from"], (d["salary"])["currency"], d['created_at']+' '+url+str(i))
+            f.write(str(d["name"])+';'+str((d["salary"])["from"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+SITE_URL+str(ID)+'\n')
+            print(d["name"], (d["salary"])["from"], (d["salary"])["currency"], d['created_at']+' '+SITE_URL+str(ID))
         else:
-            f.write(str(d["name"])+';'+str((d["salary"])["from"])+';'+str((d["salary"])["to"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+url+str(i)+'\n')            
-            print(d["name"], (d["salary"])["from"], '-' ,(d["salary"])["to"], (d["salary"])["currency"], d['created_at']+' '+url+str(i))
+            f.write(str(d["name"])+';'+str((d["salary"])["from"])+';'+str((d["salary"])["to"])+';'+str((d["salary"])["currency"])+';'+str(d['created_at'])+';'+SITE_URL+str(ID)+'\n')  
+            print(d["name"], (d["salary"])["from"], '-' ,(d["salary"])["to"], (d["salary"])["currency"], d['created_at']+' '+SITE_URL+str(ID))
+
     except:
         time.sleep(random.uniform(0, 0.1))
     f.close()
